85933d48114e13bde557232f3312d8d7971040fe
Robert Avetisyan <robertoxxx93@gmail.com>
2020-07-02 12:01:18 +0400

    empty test commit

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,7 +9,7 @@ class CommandTest(TestCase):
 
     def test_wait_for_db_ready(self):
         """Test waiting or the db when db is available"""
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as  gi:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
