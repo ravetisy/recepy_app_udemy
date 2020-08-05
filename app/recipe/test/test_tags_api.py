752dd836f6a9ebf6fc9c201c6feae0684158e45f
Robert Avetisyan <robertoxxx93@gmail.com>
2020-08-05 17:11:29 +0400

    Added create tag feature, overriden from createModelMixins

--- app/recipe/test/test_tags_api.py
+++ app/recipe/test/test_tags_api.py
@@ -70,3 +70,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 2)
         self.assertEqual(res.data[1]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating new tag for authorized user"""
+        payload = {'name': 'Testname'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating tag with empty string"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
